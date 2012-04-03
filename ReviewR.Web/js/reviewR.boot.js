434a2f39f5e57aaa10b27a5d2022e25f03f65457
anurse <andrew@andrewnurse.net>
2012-04-03 15:21:51 -0700

    Started dramatically simplifying the code. Basic infrastructure in place

---
+++ ReviewR.Web/js/reviewR.boot.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="reviewR.js" />
+
+// reviewR.boot.js
+// Startup code
+
+if (!window.rR) {
+    throw 'reviewR.js must be imported before reviewR.boot.js';
+}
+
+(function (rR) {
+    "use strict";
+
+    // To avoid users being able to call methods using url hacking, we have a list of known controllers:
+    var _root = '/';
+
+    // Public methods
+    function start(root, env) {
+        _root = root;
+
+        var homePage = rR.models.page({
+            view: 'Home.Index',
+            model: {}
+        });
+
+        var viewModel = rR.models.system({
+            environment: env,
+            activePage: homePage
+        });
+
+        ko.applyBindings(viewModel, document.body);
+    }
+
+    $.extend(rR, {
+        start: start
+    });
+})(window.rR);
---
+++ ReviewR.Web/js/reviewR.js
@@ -0,0 +1,74 @@
+﻿// reviewR.js
+// Top-level view models
+
+if (!window.rR) {
+    window.rR = {}
+}
+
+(function (rR) {
+    "use strict";
+
+    // User View model
+    function user(source) {
+        var exports = {};
+        source = source || {};
+
+        // Fields
+        exports.id = ko.observable(source.id || 0);
+        exports.email = ko.observable(source.email || '');
+        exports.displayName = ko.observable(source.displayName || '');
+        exports.roles = ko.observableArray(source.roles || []);
+        exports.loggedIn = ko.observable(source.loggedIn || false);
+        exports.isAdmin = ko.computed(function () {
+            return _.indexOf(currentUser.roles(), 'admin') > -1;
+        });
+
+        return exports;
+    }
+
+    // Page view model
+    function page(source) {
+        var exports = {};
+        source = source || {};
+
+        // Fields
+        exports.view = ko.observable(source.view || '');
+        exports.model = ko.observable(source.model || {});
+        exports.start = ko.observable(source.start);
+        exports.templateId = ko.computed(function () { return 't:' + exports.view(); });
+
+        return exports;
+    }
+
+    // System view model
+    function system(source) {
+        var exports = {};
+        source = source || {};
+
+        // Fields
+        exports.environment = ko.observable(source.environment || '');
+        exports.currentUser = ko.observable(source.currentUser || {});
+        exports.activePage = ko.observable(source.activePage || page());
+        exports.isDev = ko.computed(function () { exports.environment() === 'Development'; });
+        exports.isTest = ko.computed(function () { exports.environment() === 'Test'; });
+        exports.isProd = ko.computed(function () { exports.environment() === 'Production'; });
+        exports.isStage = ko.computed(function () { exports.environment() === 'Staging'; });
+
+        // Actions
+        exports.startLogin = function () {
+        };
+
+        exports.startRegister = function () {
+        }
+
+        return exports;
+    }
+
+    $.extend(rR, {
+        models: {
+            user: user,
+            system: system,
+            page: page
+        }
+    });
+})(window.rR);
