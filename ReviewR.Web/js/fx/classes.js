cdbafa6f0d5ed067fb2e84d2cd04ec6e013a56ca
anurse <andrew@andrewnurse.net>
2012-04-11 20:57:38 -0700

    Started tinkering with a reworked framework

---
+++ ReviewR.Web/js/fx/classes.js
@@ -0,0 +1,72 @@
+﻿var classes = (function (undefined) {
+    "use strict";
+    var exports = {};
+
+    function publishSymbol(target, name, impl, writable) {
+        if (Object.defineProperty) {
+            Object.defineProperty(target, name, {
+                value: impl,
+            });
+        } else {
+            target[name] = impl;
+        }
+    }
+
+    function mix(obj, mixin) {
+        /// <param name="obj" type="Object" />
+        /// <param name="mixin" type="Function" />
+        mixin.apply(obj, []);
+    }
+
+    function mixin(contract, definition) {
+        /// <param name="contract" type="Function" />
+        /// <param name="definition" type="Function" />
+        if (definition === undefined) {
+            definition = contract;
+            contract = function () { return {}; };
+        }
+
+        // vsdoc_only
+        definition.apply(contract(), []); // For design-time support.
+        // end vsdoc_only
+        return function () {
+            definition.apply(this, []);
+        };
+    }
+
+    function namespace(name, definition) {
+        var ns = {};
+        if (typeof definition === "function") {
+            var ret = definition(ns);
+            if (ret) {
+                ns = ret;
+            }
+        } else {
+            ns = definition;
+        }
+
+        var parts = name.split('.');
+        var current = window;
+        for (var i = 0; i < parts.length; i++) {
+            if (!current[parts[i]]) {
+                current[parts[i]] = {};
+            }
+            current = current[parts[i]];
+        }
+
+        for (var key in ns) {
+            if (ns.hasOwnProperty(key)) {
+                current[key] = ns[key];
+            }
+        }
+    }
+
+    publishSymbol(exports, 'mixin', mixin);
+    publishSymbol(exports, 'mix', mix);
+    publishSymbol(exports, 'namespace', namespace);
+
+    return exports;
+})();
+
+// Define classes namespace using classes!
+classes.namespace('classes', classes);
---
+++ ReviewR.Web/js/fx/modularious.js
@@ -0,0 +1,97 @@
+﻿/// <reference path="../../Scripts/signals.js" />
+/// <reference path="classes.js" />
+/// <reference path="Backbone.lite.js" />
+
+// modularious - a system for modular JavaScript application development
+(function (undefined) {
+    classes.namespace('modularious', function (ns) {
+        function moduleHostContract() {
+            return {
+            };
+        };
+        ns.module = classes.mixin(moduleHostContract, function () {
+            var self = this;
+            this.attach = function (app) {
+                self.app = app;
+            }
+        });
+
+        function autoconfig(key) {
+            return document.documentElement.getAttribute('data-' + key);
+        }
+
+        ns.App = function (rootUrl, environment) {
+            var self = this;
+            var _rootUrl = rootUrl || autoconfig('root');
+            var _environment = environment || autoconfig('environment');
+            var _modules = [];
+
+            self.router = new modularious.routing.Router();
+            self.mapRoute = self.router.map;
+            
+            self.module = function (module) {
+                _modules.push(module);
+            };
+
+            self.start = function () {
+                for (var i = 0; i < _modules.length; i++) {
+                    _modules[i].attach(self);
+                }
+
+                self.router.start(_rootUrl);
+            };
+        };
+
+        ns.Signal = function () {
+        }
+        ns.Signal.prototype = new signals.Signal();
+        ns.Signal.prototype.asListener = function () {
+            var self = this;
+            return {
+                add: function (listener, listenerContext, priority) { self.add(listener, listenerContext, priority); },
+                addOnce: function (listener, listenerContext, priority) { self.addOnce(listener, listenerContext, priority); },
+                remove: function (listener, context) { self.remove(listener, context); },
+            };
+        }
+    });
+
+    classes.namespace('modularious.routing', function (ns) {
+        ns.Router = function () {
+            var self = this;
+            var _history = new Backbone.History();
+            var _router = new Backbone.Router();
+
+            self.map = function (name, url, handler) {
+                _router.route(url, name, handler);
+            };
+
+            self.start = function (root) {
+                _history.start({ root: root });
+            };
+
+            self.stop = function () {
+                _history.stop();
+            }
+        };
+    });
+
+    classes.namespace('modularious.rendering', function (ns) {
+        var jqTemplateEngine = {
+            renderTemplate: function (host, template) { },
+            lookupTemplate: function (id) { }
+        };
+
+        ns.ViewHost = function (element) {
+            var self = this;
+            var _hostElement = element;
+            var _removing = new modularious.Signal();
+            var _injecting = new modularious.Signal();
+
+            self.removing = _removing.asListener();
+            self.injecting = _injecting.asListener();
+
+            self.setView = function (name, element) {
+            };
+        };
+    });
+})();
